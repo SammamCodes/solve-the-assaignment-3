3f6d8027cf72c4a2689a4ac431734ee4f5f77c4a
sammamcodes <sammamabdullahalsuhaimee@gmail.com>
2024-08-26 21:11:05 +0800

    Commit message describing the changes

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        backgroundSize: {
+          'custom-size': '1440px 909.83px',
+        },
+      },
+    },
+  };
+  module.exports = {
+    theme: {
+      extend: {
+        padding: {
+          '63.21': '63.21%',
+        },
+      },
+    },
+  };
+  module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+          'hero-pattern': "url('bg.jpg')",
+          'footer-texture': "url('bg.jpg')",
+        }
+      }
+    }
+  }
